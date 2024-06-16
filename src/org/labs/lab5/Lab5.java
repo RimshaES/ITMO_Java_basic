988c5576cbbbfcc3da778a6fc658507e94413430
Elena Rimsha <rimshalena@gmail.com>
2024-06-16 19:49:46 +0200

    lab5

---
+++ src/org/labs/lab5/Lab5.java
@@ -0,0 +1,60 @@
+package org.labs.lab5;
+
+public class Lab5 {
+    public static String findLongest(String sentence) {
+        if (sentence == null || sentence.isEmpty()) {
+            return "";
+        }
+        String longestWord = "";
+        String[] words = sentence.split(" ");
+        for (String word : words) {
+            if (longestWord.length() < word.length()) {
+                longestWord = word;
+            }
+        }
+        System.out.println(longestWord);
+        return longestWord;
+    }
+
+    public static boolean checkPalindrom(String word) {
+        char[] chars = word.toCharArray();
+        int start = 0;
+        int end = chars.length - 1;
+        while (start <= end) {
+            if (chars[start] != chars[end]) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+
+    public static String changeLetters(String text) {
+        return text.replaceAll("бяка", "[вырезано цензурой]");
+    }
+
+    public static int countOverlap(String text, String substring) {
+        int count = 0;
+        int length = substring.length();
+        int lengthText = text.length();
+        for (int i = 0; i < lengthText - length; i++) {
+            String s = text.substring(i);
+            if (s.startsWith(substring)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static String doReverse(String text) {
+        System.out.println("The given string is: " + text);
+        String result = "";
+        String[]  words = text.split(" ");
+        for (String word : words) {
+            result += new StringBuilder(word).reverse() + " ";
+        }
+        System.out.println("The string reversed word by word is: ");
+        return result;
+    }
+}
---
+++ src/org/labs/lab5/Main.java
@@ -0,0 +1,12 @@
+package org.labs.lab5;
+
+public class Main {
+    public static void main(String[] args) {
+        Lab5.findLongest("Я собираюсь идти гулять.");
+        System.out.println(Lab5.checkPalindrom("jkikj"));
+        System.out.println(Lab5.changeLetters("ты бяка и супербяка"));
+        System.out.println(Lab5.countOverlap("трава была зеленая трава была мокрая трава была вкусная",
+        "трава"));
+        System.out.println(Lab5.doReverse("This is a test string"));
+    }
+}
