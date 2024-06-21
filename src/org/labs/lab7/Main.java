21f1d2b71772cd26203a7d653b8f2d3d39cdd1d8
Elena Rimsha <rimshalena@gmail.com>
2024-06-21 12:11:24 +0200

    lab7

---
+++ src/org/labs/lab7/Main.java
@@ -0,0 +1,12 @@
+package org.labs.lab7;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Plane plane1 = new Plane();
+        Plane plane2 = new Plane();
+        plane1.classWingCall(2000);
+        plane2.classWingCall(3000);
+    }
+}
