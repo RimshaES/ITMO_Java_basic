19eeb116b8617a64216095ed9ba4aa234d9abd5a
Elena Rimsha <rimshalena@gmail.com>
2024-07-21 18:26:14 +0200

    lab11/ исправление sleep на CountDownLatch

--- src/org/labs/lab11/MultiCounter.java
+++ src/org/labs/lab11/MultiCounter.java
@@ -1,26 +1,34 @@
 package org.labs.lab11;
 
+import java.util.concurrent.CountDownLatch;
+
 public class MultiCounter implements Runnable {
     private Counter counter;
+    private CountDownLatch latch;
 
-    public MultiCounter(Counter counter) {
+    public MultiCounter(Counter counter, CountDownLatch latch) {
         this.counter = counter;
+        this.latch = latch;
     }
 
     @Override
     public void run() {
         for (int i = 0; i < 100; i++) {
             counter.increment();
+            latch.countDown();
         }
     }
 
     public static void main(String[] args) {
+        CountDownLatch latch = new CountDownLatch(100000);
+
         Counter count = new Counter();
+
         for (int i = 0; i < 1000 ; i++) {
-           new Thread(new MultiCounter(count)).start();
+           new Thread(new MultiCounter(count, latch)).start();
         }
         try {
-            Thread.sleep(1000);
+            latch.await();
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
